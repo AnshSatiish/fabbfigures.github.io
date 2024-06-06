5bc9b611f2765b680b7311b2008ce4fbde9a84d6
AnshSatiish <169073333+AnshSatiish@users.noreply.github.com>
2024-06-06 12:08:45 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     hamburger.addEventListener('click', () => {
         navMenu.classList.toggle('show');
+        hamburger.classList.toggle('open');
     });
 });
 
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
+function toggleMobileMenu() {
+    const navMenu = document.querySelector('nav ul');
+    navMenu.classList.toggle('show');
+    document.getElementById('hamburger-icon').classList.toggle('open');
 }
